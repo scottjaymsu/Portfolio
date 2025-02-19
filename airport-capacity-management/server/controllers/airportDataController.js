bb46fb606524eaff46f7354b390fc6ed3855ca57
Jay <scottjay@msu.edu>
2025-02-18 22:23:42 -0500

    controller for current airport capacity

--- airport-capacity-management/server/controllers/airportDataController.js
+++ airport-capacity-management/server/controllers/airportDataController.js
@@ -23,3 +23,23 @@ exports.getAirportData = (req, res) => {
         res.json(results);
     });
 }
+
+// Controller to get current capacity of airport
+exports.getCurrentCapacity = (req, res) => {
+    // Testing
+    const ident = req.params.ident || 'KTEB';
+
+    const query = 'SELECT COUNT(*) FROM netjets_fleet JOIN flight_plans ON netjets_fleet.flightRef = flight_plans.flightRef WHERE flight_plans.arrival_airport = ? AND flight_plans.arrived = TRUE';
+
+    airportDB.query(query, [ident], (err, results) => {
+        if (err) {
+            console.error("Error querying airport data.", err);
+            return res.status(500).json({ error: 'Error querying airport data.' });
+        }
+
+        // Testing
+        console.log(results);
+        // Send results back as response
+        res.json(results);
+    });
+}
--- airport-capacity-management/server/routes/airportDataRoutes.js
+++ airport-capacity-management/server/routes/airportDataRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const airportDataController = require('../controllers/AirportDataController');
 
 router.get('/getAirportData/ident', airportDataController.getAirportData);
+router.get('/getCurrentCapacity/ident', airportDataController.getCurrentCapacity);
 
 module.exports = router;
