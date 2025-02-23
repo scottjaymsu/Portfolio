2badd5c7279cf73eeb76687894cd2bd476d2eab2
emtelgen <telgen12@msu.edu>
2025-02-23 10:51:24 -0500

    deleted unused so no errors pop up

--- airport-capacity-management/server/routes/batchFileRoutes.js
+++ airport-capacity-management/server/routes/batchFileRoutes.js
@@ -3,8 +3,6 @@ const batchFileController = require('../controllers/batchFileController');
 
 const router = express.Router();
 
-router.post('/insertBatchData', batchFileController.insertBatchData);
-router.post('/insertAirportData', batchFileController.insertAirportData);
 router.post('/insertAirport', batchFileController.insertAirport);
 router.post('/getExistingAirports', batchFileController.getExistingAirports);
 
