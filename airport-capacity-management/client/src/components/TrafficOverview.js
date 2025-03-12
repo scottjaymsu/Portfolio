4fac3b686e9788427de77f80231516514c45fd94
Jay <scottjay@msu.edu>
2025-03-12 15:50:17 -0400

    Departing and Arriving flights quantified by day -- data is taking a while to pull in?

--- airport-capacity-management/client/src/components/TrafficOverview.js
+++ airport-capacity-management/client/src/components/TrafficOverview.js
@@ -26,10 +26,10 @@ export default function TrafficOverview({id}) {
     const [departingFlights, setDepartingFlights] = useState([]);
     // State to hold arriving flights
     const [arrivingFlights, setArrivingFlights] = useState([]);    
-    // State to hold parked planes 
-    const [parkedPlanes, setParkedPlanes] = useState([]);
-    // State to hold planes under maintenance
-    const [planesUnder, setPlanesUnder] = useState([]);
+    // // State to hold parked planes 
+    // const [parkedPlanes, setParkedPlanes] = useState([]);
+    // // State to hold planes under maintenance
+    // const [planesUnder, setPlanesUnder] = useState([]);
     // State to hold error message
     const [error, setError] = useState('');
 
@@ -41,10 +41,10 @@ export default function TrafficOverview({id}) {
                 setDepartingFlights(response.data);
             })
             .catch((err) => {
-                setError('Error fetching departing flights');
-                console.error('Error fetching departing flights:', err);
+                setError(err);
+                console.error('Error fetching departing flights:', error);
             });
-    }, []);
+    }, [id, error]);
 
     // Fetch arriving flights by faa designator when component mounts
     useEffect(() => {
@@ -54,29 +54,82 @@ export default function TrafficOverview({id}) {
                 setArrivingFlights(response.data);
             })
             .catch((err) => {
-                setError('Error fetching arriving flights');
-                console.error('Error fetching arriving flights:', err);
+                setError(err);
+                console.error('Error fetching arriving flights:', error);
             });
-    }, []);  
-    
-    // Fetch parked planes by faa designator when component mounts
-    useEffect(() => {
-        // Fetch parked planes by airport 
-        axios.get(`http://localhost:5001/airportData/getParkedPlanes/${id}`)
-            .then((response) => {
-                setParkedPlanes(response.data);
- 
-            })
-            .catch((err) => {
-                setError('Error fetching parked planes');
-                console.error('Error fetching parked planes:', err);
-            });
-    }, []);
+    }, [id, error]);  
 
-    // Set random value for maintence (0 - # of parked planes - 2)
-    useEffect(() => {
-        setPlanesUnder(Math.floor(Math.random() * parkedPlanes.length));        
-    }, [parkedPlanes]);  
+    // Get data in "MM-DD" format from ISO string
+    const getFormattedDate = (d) => {
+        // null values
+        if (!d) return '';
+
+        const date = new Date(d);
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${month}-${day}`;
+    };
+
+    // Reverse sort flights by their ETA for arriving flights and ETD for departing flights
+    const reverseFlightsByDate = (flights, key) => {
+        return flights.sort((a, b) => {
+            if (!a[key] || !b[key]) return 0;
+            return new Date(b[key]) - new Date(a[key]);
+        });
+    };
+
+    // Reverse sort the flights
+    const sortedDepartingFlights = reverseFlightsByDate(departingFlights, 'etd');
+    const sortedArrivingFlights = reverseFlightsByDate(arrivingFlights, 'eta');    
+    
+    // Count the number of arriving flights by date
+    const countArriving = (flights) => {
+        return flights.reduce((count, flight) => {
+            // null eta
+            if (!flight.eta) return count;
+
+            const date = getFormattedDate(flight.eta);
+            count[date] = count[date] ? count[date] + 1 : 1;
+            return count;
+        }, {});
+    };
+
+    // Count the number of departing flights by date    
+    const countDeparting = (flights) => {
+        return flights.reduce((count, flight) => {
+            // null etd
+            if (!flight.etd) return count;
+            
+            const date = getFormattedDate(flight.etd);
+            count[date] = count[date] ? count[date] + 1 : 1;
+            return count;
+        }, {});
+    };
+    
+     // Count the arriving and departing flights by date
+    const departingCount = countDeparting(sortedDepartingFlights);
+    const arrivingCount = countArriving(sortedArrivingFlights);
+    
+    
+    
+    // // Fetch parked planes by faa designator when component mounts
+    // useEffect(() => {
+    //     // Fetch parked planes by airport 
+    //     axios.get(`http://localhost:5001/airportData/getParkedPlanes/${id}`)
+    //         .then((response) => {
+    //             setParkedPlanes(response.data);
+ 
+    //         })
+    //         .catch((err) => {
+    //             setError('Error fetching parked planes');
+    //             console.error('Error fetching parked planes:', err);
+    //         });
+    // }, []);
+
+    // // Set random value for maintence (0 - # of parked planes - 2)
+    // useEffect(() => {
+    //     setPlanesUnder(Math.floor(Math.random() * parkedPlanes.length));        
+    // }, [parkedPlanes]);  
     
     /**
      * Create bar chart to display quantitive info
@@ -89,30 +142,27 @@ export default function TrafficOverview({id}) {
      * }
      */
 
-    // Data in the bar chart
-    const chartData = [
-        {
-            category : "Traffic Overview",
-            Arriving : arrivingFlights.length,
-            Departing : departingFlights.length,
-            Parked : parkedPlanes.length,
-            Maintenance : planesUnder
-        }
-    ];
+    
+    // Combine the data for the chart and table
+    const dates = [...new Set([...Object.keys(departingCount), ...Object.keys(arrivingCount)])];
+    const chartData = dates.map(date => ({
+        category: date,
+        Arriving: arrivingCount[date],
+        Departing: departingCount[date]
+    }));
+
+    // // Data in the bar chart
+    // const chartData = [
+    //     {
+    //         category : "Traffic Overview",
+    //         Arriving : arrivingFlights.length,
+    //         Departing : departingFlights.length
+    //     }
+    // ];
 
     return (
         <div>
-            <table style={{width: '100%', marginBottom: '20px', borderCollapse: 'collapse'}}>
-                <thead>
-                    <tr>
-                        <th colSpan={4} style={{textAlign: 'center', padding: '10px', backgroundColor: '#1e3a8a', fontSize: '18px', fontWeight: 'bold'}}>
-                            Traffic Overview
-                        </th>
-                    </tr>
-                </thead>
-            </table>
-
-            <ResponsiveContainer width="100%" height={150}>
+            <ResponsiveContainer width="100%" height={300}>
                 <BarChart data={chartData}>               
                     <XAxis dataKey="category" />
                     <YAxis />
@@ -120,8 +170,6 @@ export default function TrafficOverview({id}) {
                     <Legend />
                     <Bar dataKey="Arriving" fill="rgb(88,120,163)" />
                     <Bar dataKey="Departing" fill="rgb(228,147,67)" />
-                    <Bar dataKey="Parked" fill="rgb(99, 188, 182)" />
-                    <Bar dataKey="Maintenance" fill="rgb(209,96,94)" />
                 </BarChart>
             </ResponsiveContainer>
         </div>
