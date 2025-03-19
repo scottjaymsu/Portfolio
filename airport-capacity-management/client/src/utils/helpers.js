b073118c1e969ce81c811c53636365e23da75171
ryannseymour <ryannseymour3@gmail.com>
2025-03-19 18:52:00 -0400

    fix override of marker colors with last change

--- airport-capacity-management/client/src/utils/helpers.js
+++ airport-capacity-management/client/src/utils/helpers.js
@@ -1,14 +1,25 @@
-export const getStatusColor  = (parking_taken, total_parking) => {
-  const status = parking_taken / total_parking;
-  if (status == 1) {
-    // Red - overcapacity
-    return "#c76666";
-  } else if (status >= 0.8) {
-    // Orange - approaching capacity
-    return "#ecac57";
-  } else {
-    // Green - open
-    return "#b9be80";
+export const getStatusColor = (parking_taken_or_status, total_parking) => {
+  // If parking_taken_or_status is a string, handle it as a status message
+  if (typeof parking_taken_or_status === "string") {
+    switch (parking_taken_or_status) {
+      case "Overcapacity":
+        return "#c76666"; // red
+      case "Approaching capacity":
+        return "#ecac57"; // orange
+      default:
+        return "#b9be80"; // green or default
+    }
+  } 
+  // If parking_taken_or_status is a number, calculate the status based on the ratio
+  else {
+    const status = parking_taken_or_status / total_parking;
+    if (status === 1) {
+      return "#c76666"; // red
+    } else if (status >= 0.8) {
+      return "#ecac57"; // orange
+    } else {
+      return "#b9be80"; // green
+    }
   }
 };
 
