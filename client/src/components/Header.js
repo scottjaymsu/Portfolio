7bab23273a3ec2db7099e3964c4632fa84ac5309
Jay <scottjay@msu.edu>
2025-03-21 17:23:26 -0400

    Header UI for homepage -- need to add functionality to links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,15 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h1>Header</h1>
+            <div className="header-name">
+                <p style={{fontWeight: "bold"}}>Jay Scott Jr.</p>
+                <p style={{ fontSize: "20px" }}>Software Engineer</p>
+            </div>               
+            <div className="header-links">
+                <a href="#">Resume</a>
+                <a href="#">Projects</a>
+                <a href="#">Email</a>
+            </div>       
         </div>
     );
 };
