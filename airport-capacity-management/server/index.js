fead662c10761d383dd5a0f4a02ee7d8ad3b5f15
Jay <scottjay@msu.edu>
2025-03-27 07:17:55 -0400

    ssh tunnel created in node

--- airport-capacity-management/server/index.js
+++ airport-capacity-management/server/index.js
@@ -11,6 +11,7 @@ const alertRoutes = require('./routes/alertRoutes');
 const mapRoutes = require('./routes/mapRoutes');
 const parkingPriorityRoutes = require('./routes/parkingpriorityRoutes');
 const fboRoutes = require('./routes/fboRoutes');
+const { spawn } = require('child_process');
 
 const port = process.env.PORT || 5001;
 
@@ -28,10 +29,36 @@ app.use('/alerts', alertRoutes);
 app.use('/airportsPriority', parkingPriorityRoutes);
 app.use('/airports/fbo', fboRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Start SSH Tunnel
+const sshTunnelCommand = `ssh`;
+const sshTunnelArgs = [
+  '-v',
+  '-i', `"netjets-db-manager-key 1.pem"`,
+  '-N',
+  '-L',
+  '50173:netjets-planes-and-airports.czkio0gye9rr.us-east-2.rds.amazonaws.com:3306',
+  'ec2-user@ec2-3-16-169-179.us-east-2.compute.amazonaws.com'
+];
+
+const sshTunnel = spawn(sshTunnelCommand, sshTunnelArgs, { shell: true });
+
+sshTunnel.stdout.on('data', (data) => {
+  console.log(`SSH Tunnel: ${data}`);
 });
 
+sshTunnel.stderr.on('data', (data) => {
+  console.error(`SSH Tunnel Error: ${data}`);
+});
+
+sshTunnel.on('close', (code) => {
+  if (code === 0) {
+    console.log('SSH Tunnel established successfully');
+  } else {
+    console.log(`SSH Tunnel process exited with code ${code}`);
+  }
+});
+
+// Start the Express server after SSH tunnel is established
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
